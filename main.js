29ba26facd038df6e99c55b1ded8069072c8184c
Dhriti-Malhotra <134493853+Dhriti-Malhotra@users.noreply.github.com>
2024-03-06 18:14:07 +1100

    Add files via upload

--- main.js
+++ main.js
@@ -20,10 +20,26 @@ function gotPoses(results)
     if(results.length > 0)
     {
         console.log(results);
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        console.log("noseX = " + noseX + "noseY = " + noseY);
+
+        rightWristX = results[0].pose.rightWrist.x;
+        leftWristX = results[0].pose.leftWrist.x;
+        difference = floor(leftWristX - rightWristX);
     }
 }
 
 function draw()
 {
     background('##DDA0DD');
+    fill('#B0C4DE');
+    stroke('#B0E0E6');
+    Text('Dhriti', noseX, noseY)
 }
+
+rightWristX = 0;
+leftWristX = 0;
+difference = 0;
+noseX = 20;
+noseY = 200;
